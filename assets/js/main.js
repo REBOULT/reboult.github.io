e36ae1f400fcf459a19f58dc4e9db9e1f28a4ddb
sero <108736140+REBOULT@users.noreply.github.com>
2024-10-25 22:03:25 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ const button = document.querySelector(".preloader-content_button");
 let animInterval;
 let loadInterval;
 
-const videoId = "nya0MmsbmVw" // U-IK9qtBspg
+const videoId = "E8eYh6lLcVg" // U-IK9qtBspg
 YT.ready(_=>{
     window.player = new YT.Player("player", {
         videoId: videoId,
